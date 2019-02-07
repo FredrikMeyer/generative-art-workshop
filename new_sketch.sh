5c19da478d61074147ca603ef64fa4baeb26be12
Fredrik Meyer <hrmeyer@gmail.com>
2019-02-07 19:26:27 +0100

    Update script

--- new_sketch.sh
+++ new_sketch.sh
@@ -4,9 +4,13 @@ if [ -z "$1" ]
 then
   echo "Please provide a folder name."
 else
-  echo "Creating sketch in folder: $1"
-  cp -r basis/ $1
-  echo "Run npm start, and open localhost:3000/$1"
+  if [ -d $1 ]; then
+     echo "Reusing folder: $1"
+  else
+    echo "Creating sketch in folder: $1"
+    cp -r basis/ $1
+  fi
+  echo "Open localhost:9966 in your browser, edit \"$1/index.js\" in your editor"
   echo "Happy sketching :-)"
 fi
 
